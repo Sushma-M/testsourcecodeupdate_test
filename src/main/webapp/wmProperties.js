767ca7661dbfbed7620a4d3f093c8d7855663f13
root <root@0a468c0d7c82>
2016-10-24 09:34:41 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "testsourcecodeupdate",
+  "homePage" : "Main",
+  "name" : "testsourcecodeupdate",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
